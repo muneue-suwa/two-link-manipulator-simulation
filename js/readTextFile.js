6f8be16e9eb4efd8f61319c506f708150c28db29
Muneue SUWA <m_suwa@s.okayama-u.ac.jp>
2022-04-12 01:04:10 +0900

    Number型のArrayとして，torqueArrayが出力されるようにした

--- js/readTextFile.js
+++ js/readTextFile.js
@@ -1,11 +1,23 @@
 // ファイルを選択するためのinput要素と、選択したファイルのURLを差し込むためのimg要素をそれぞれ取得
 const inputfile = document.getElementById('myfile');
 const log = document.getElementById('log');
+const torqueArray = [];
 
-inputfile.addEventListener('change', function(e) {
+inputfile.addEventListener('change', (e) => {
   const file = e.target.files;
   file[0].text().then(
       (text) => {
+        const lines = text.split(/\n|\r\n/);
+        let isFirstLine = true;
+        for (const line of lines) {
+          if (isFirstLine === true | line.length === 0) {
+            isFirstLine = false;
+            continue;
+          }
+          const torqueStr = line.split(',');
+          const torque = [parseFloat(torqueStr[0]), parseFloat(torqueStr[1])];
+          torqueArray.push(torque);
+        }
         log.innerText = text;
       },
   );
