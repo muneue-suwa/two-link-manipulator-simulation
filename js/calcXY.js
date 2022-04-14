86db3dffeed84c674c62dfb60eea72558c91fa79
Muneue SUWA <m_suwa@s.okayama-u.ac.jp>
2022-04-14 20:14:42 +0900

    class Manipulator を作成した

--- js/calcXY.js
+++ js/calcXY.js
@@ -1,98 +1,135 @@
 /* eslint-disable require-jsdoc */
 /* eslint-disable no-unused-vars */
-function calcXY(torqueArray) {
-  console.log('torqueArray', torqueArray);
-  const te = 15;
-  const dt = 1 / 100;
-  const nt = te / dt + 1;
+class Manipulator {
+  constructor(
+      te = 15, dt = 1 / 100,
+      th1 = Math.PI / 2, th2 = Math.PI,
+      dth1 = 0, dth2 = 0,
+      m1 = 1, m2 = 1,
+      l1 = 0.5, l2 = 0.3,
+      D1 = 3, D2 = 3,
+  ) {
+    this.te = te;
+    this.dt = dt;
 
-  let th1 = Math.PI / 2;
-  let th2 = Math.PI;
-  let dth1 = 0;
-  let dth2 = 0;
-  const m1 = 1;
-  const m2 = 1;
-  const l1 = 0.5;
-  const l2 = 0.3;
+    this.th1 = th1;
+    this.th2 = th2;
+    this.dth1 = dth1;
+    this.dth2 = dth2;
 
-  const D1 = 3;
-  const D2 = 3;
+    this.m1 = m1;
+    this.m2 = m2;
+    this.l1 = l1;
+    this.l2 = l2;
 
-  const MD = math.matrix([[D1 + D2, -D2], [-D2, D2]]);
+    this.D1 = D1;
+    this.D2 = D2;
+  }
 
-  const g = 9.8;
-  const I1 = 1 / 3 * m1 * l1 ** 2;
-  const I2 = 1 / 3 * m2 * l2 ** 2;
-  const J1 = I1 + (m1 + 4 * m2) * l1 ** 2;
-  const J2 = I2 + m2 * l2 ** 2;
+  calcPositionPerFrame(torqueArray, fps) {
+    const [dtXy1, dtXy2] = this.calcPositionPerDt(torqueArray);
+    const frameXy1 = new Array(this.te * fps);
+    const frameXy2 = new Array(this.te * fps);
+    for (let i = 0; i < frameXy1.length; i++) {
+      frameXy1[i] = dtXy1[Math.trunc(i / (fps * this.dt))];
+      frameXy2[i] = dtXy2[Math.trunc(i / (fps * this.dt))];
+    }
+    return [frameXy1, frameXy2];
+  }
 
-  const beta = 2 * m2 * l1 * l2;
+  calcPositionPerDt(torqueArray) {
+    const dt = this.dt;
+    const nt = this.te / dt + 1;
 
-  const x0 = math.matrix([[th1], [th2], [dth1], [dth2]]);
-  let x = x0;
+    let th1 = this.th1;
+    let th2 = this.th2;
+    let dth1 = this.dth1;
+    let dth2 = this.dth2;
 
-  const xy1 = new Array(nt);
-  const x1 = 2 * l1 * math.sin(th1);
-  const y1 = 2 * l1 * math.cos(th1);
-  xy1[0] = [x1, y1];
+    const m1 = this.m1;
+    const m2 = this.m2;
+    const l1 = this.l1;
+    const l2 = this.l2;
 
-  const xy2 = new Array(nt);
-  const x2 = 2 * l1 * math.sin(th1) + 2 * l2 * math.sin(th2);
-  const y2 = 2 * l1 * math.cos(th1) + 2 * l2 * math.cos(th2);
-  xy2[0] = [x2, y2];
+    const D1 = this.D1;
+    const D2 = this.D2;
 
-  for (let k = 1; k < nt; k++) {
-    const lth1 = dth1;
-    const lth2 = dth2;
-    J = math.matrix([
-      [J1, beta * math.cos(th2 - th1)], [beta * math.cos(th1 - th2), J2],
-    ]);
-    const vldth = math.multiply(
-        math.inv(J),
-        math.add(
-            math.matrix([[torqueArray[k-1][0]], [torqueArray[k-1][1]]]),
-            math.multiply(
-                -1,
-                math.matrix([
-                  [beta * math.sin(th1 - th2) * dth2 ** 2],
-                  [beta * math.sin(th2 - th1) * dth1 ** 2],
-                ]),
-            ),
-            math.multiply(
-                -1,
-                math.multiply(
-                    MD, math.matrix([[dth1], [dth2]]),
-                ),
-            ),
-            math.matrix([
-              [(m1 + 2 * m2) * g * l1 * math.sin(th1)],
-              [m2 * g * l2 * math.sin(th2)],
-            ]),
-        ),
-    );
+    const MD = math.matrix([[D1 + D2, -D2], [-D2, D2]]);
 
-    const ldth1 = vldth.subset(math.index(0, 0));
-    const ldth2 = vldth.subset(math.index(1, 0));
-    const nx = math.add(
-        x, math.multiply(
-            math.matrix([[lth1], [lth2], [ldth1], [ldth2]]), dt,
-        ),
-    );
+    const g = 9.8;
+    const I1 = 1 / 3 * m1 * l1 ** 2;
+    const I2 = 1 / 3 * m2 * l2 ** 2;
+    const J1 = I1 + (m1 + 4 * m2) * l1 ** 2;
+    const J2 = I2 + m2 * l2 ** 2;
 
-    th1 = nx.subset(math.index(0, 0));
-    th2 = nx.subset(math.index(1, 0));
-    dth1 = nx.subset(math.index(2, 0));
-    dth2 = nx.subset(math.index(3, 0));
+    const beta = 2 * m2 * l1 * l2;
 
+    const x0 = math.matrix([[th1], [th2], [dth1], [dth2]]);
+    let x = x0;
+
+    const xy1 = new Array(nt);
     const x1 = 2 * l1 * math.sin(th1);
     const y1 = 2 * l1 * math.cos(th1);
-    xy1[k] = [x1, y1];
+    xy1[0] = [x1, y1];
 
+    const xy2 = new Array(nt);
     const x2 = 2 * l1 * math.sin(th1) + 2 * l2 * math.sin(th2);
     const y2 = 2 * l1 * math.cos(th1) + 2 * l2 * math.cos(th2);
-    xy2[k] = [x2, y2];
+    xy2[0] = [x2, y2];
 
-    x = nx;
+    for (let k = 1; k < nt; k++) {
+      const lth1 = dth1;
+      const lth2 = dth2;
+      const J = math.matrix([
+        [J1, beta * math.cos(th2 - th1)], [beta * math.cos(th1 - th2), J2],
+      ]);
+      const vldth = math.multiply(
+          math.inv(J),
+          math.add(
+              math.matrix([[torqueArray[k-1][0]], [torqueArray[k-1][1]]]),
+              math.multiply(
+                  -1,
+                  math.matrix([
+                    [beta * math.sin(th1 - th2) * dth2 ** 2],
+                    [beta * math.sin(th2 - th1) * dth1 ** 2],
+                  ]),
+              ),
+              math.multiply(
+                  -1,
+                  math.multiply(
+                      MD, math.matrix([[dth1], [dth2]]),
+                  ),
+              ),
+              math.matrix([
+                [(m1 + 2 * m2) * g * l1 * math.sin(th1)],
+                [m2 * g * l2 * math.sin(th2)],
+              ]),
+          ),
+      );
+
+      const ldth1 = vldth.subset(math.index(0, 0));
+      const ldth2 = vldth.subset(math.index(1, 0));
+      const nx = math.add(
+          x, math.multiply(
+              math.matrix([[lth1], [lth2], [ldth1], [ldth2]]), dt,
+          ),
+      );
+
+      th1 = nx.subset(math.index(0, 0));
+      th2 = nx.subset(math.index(1, 0));
+      dth1 = nx.subset(math.index(2, 0));
+      dth2 = nx.subset(math.index(3, 0));
+
+      const x1 = 2 * l1 * math.sin(th1);
+      const y1 = 2 * l1 * math.cos(th1);
+      xy1[k] = [x1, y1];
+
+      const x2 = 2 * l1 * math.sin(th1) + 2 * l2 * math.sin(th2);
+      const y2 = 2 * l1 * math.cos(th1) + 2 * l2 * math.cos(th2);
+      xy2[k] = [x2, y2];
+
+      x = nx;
+    }
+    return [xy1, xy2];
   }
-  return [xy1, xy2];
 }
--- js/drawRobot.js
+++ js/drawRobot.js
@@ -1,24 +1,15 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable require-jsdoc */
-const simulateTimeDiv = document.getElementById('simulateTime');
+const mode1Btn = document.getElementById('mode1-tab');
+const mode2Btn = document.getElementById('mode2-tab');
 const startSimulatorBtn = document.getElementById('startSimulator');
 const resetSimulatorBtn = document.getElementById('resetSimulator');
 const saveSimulatorBtn = document.getElementById('saveSimulator');
+const simulateTimeDiv = document.getElementById('simulateTime');
 const simulatorDiv = document.getElementById('simulator');
-
-const te = 15;
-const dt = 1 / 100;
-const nt = te / dt + 1;
 
 const canvasSize = getCanvasSize();
 const fps = 60;
-let count = 0;
-
-// 描画するxy1[j]のjを計算する
-const xyFrame = new Array(te * fps);
-for (let i = 0; i < xyFrame.length; i++) {
-  xyFrame[i] = Math.trunc(i / (fps * dt));
-}
 
 const pixelRatio = 200;
 const allowableError = 0.03;
@@ -26,14 +17,23 @@ const targetXY = [1.2, -0.8];
 
 let doDraw = false;
 let startTime;
+let count;
 let xy1;
 let xy2;
+let frameNum;
+
+let manipulator;
+mode1Btn.addEventListener('click', () => {
+  manipulator = new Manipulator(te=15, dt=1/100);
+});
+mode2Btn.addEventListener('click', () => {
+  manipulator = new Manipulator(te=10, dt=1/100);
+});
 
 startSimulatorBtn.disabled = true;
 startSimulatorBtn.addEventListener('click', () => {
-  xy1xy2 = calcXY(torqueArray);
-  xy1 = xy1xy2[0];
-  xy2 = xy1xy2[1];
+  [xy1, xy2] = manipulator.calcPositionPerFrame(torqueArray, fps);
+  frameNum = xy1.length;
   doDraw = true;
   startTime = Date.now();
   startSimulatorBtn.disabled = true;
@@ -57,7 +57,6 @@ function setup() {
   simulationCanvas.parent('simulationCanvas');
   resetSimulation();
   frameRate(fps);
-  // console.log(xy1);
   console.log('start: ', new Date());
   noLoop();
 
@@ -81,10 +80,10 @@ function draw() {
       canvasHeight / 2 - targetXY[1] * pixelRatio,
       allowableError * pixelRatio * 2,
   );
-  const x1 = pixelRatio * xy1[xyFrame[count]][0];
-  const y1 = pixelRatio * xy1[xyFrame[count]][1];
-  const x2 = pixelRatio * xy2[xyFrame[count]][0];
-  const y2 = pixelRatio * xy2[xyFrame[count]][1];
+  const x1 = pixelRatio * xy1[count][0];
+  const y1 = pixelRatio * xy1[count][1];
+  const x2 = pixelRatio * xy2[count][0];
+  const y2 = pixelRatio * xy2[count][1];
 
   strokeWeight(5); // 線幅を10pxに
   stroke(50, 50, 200);
@@ -101,7 +100,7 @@ function draw() {
   const currentTime = (Date.now() - startTime) / 1000;
   simulateTimeDiv.textContent = currentTime;
 
-  if (count === xyFrame.length) {
+  if (count === frameNum) {
     doDraw = false;
     noLoop();
     console.log('end: ', new Date());
