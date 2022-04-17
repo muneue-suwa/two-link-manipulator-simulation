6aa54a5288b059085a8fcaa137d79ae90a7c59fc
Muneue SUWA <m_suwa@s.okayama-u.ac.jp>
2022-04-17 19:01:35 +0900

    Add comment

--- js/convertCoordinates.js
+++ js/convertCoordinates.js
@@ -1,12 +1,28 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable require-jsdoc */
-class CoordinatesConverter {
+/**
+ * Convert manipulator coordinates to pixel coordinates
+ */
+class CoordinatesConverter { // eslint-disable-line no-unused-vars
+  /**
+   * Initialize class
+   *
+   * @param {Number} originX
+   * @param {Number} originY
+   * @param {Number} pixelRatio
+   */
   constructor(originX, originY, pixelRatio) {
     this.originX = originX;
     this.originY = originY;
     this.pixelRatio = pixelRatio;
   }
 
+  /**
+   * Draw manipulator arm lines
+   *
+   * @param {Number} x1 Manipulator positions x1
+   * @param {Number} y1 Manipulator positions y1
+   * @param {Number} x2 Manipulator positions x2
+   * @param {Number} y2 Manipulator positions y2
+   */
   line(x1, y1, x2, y2) {
     line(
         this.originX + x1 * this.pixelRatio,
@@ -16,6 +32,13 @@ class CoordinatesConverter {
     );
   }
 
+  /**
+   * Draw target position circle
+   *
+   * @param {Number} x Target positions x
+   * @param {Number} y Target positions y
+   * @param {Number} diameter Target allowable error
+   */
   circle(x, y, diameter) {
     circle(
         this.originX + x * this.pixelRatio,
@@ -24,6 +47,9 @@ class CoordinatesConverter {
     );
   }
 
+  /**
+   * Draw xy-axes
+   */
   drawAxes() {
     line(0, this.originY, this.originX * 2, this.originY);
     line(this.originX, 0, this.originX, this.originY * 2);
